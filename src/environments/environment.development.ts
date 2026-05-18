014880808544fd1973d0850f8e49a88c3b259233
Virtual <vaibhavthangriyal@gmail.com>
2026-05-18 21:26:14 +0530

    environment variable updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ const metaEnv = (import.meta as any)?.env ?? {};
 
 export const environment = {
   production: false,
-  apiBaseUrl: metaEnv['NG_APP_API_BASE_URL'] ?? 'http://localhost:4000/api',
+  apiBaseUrl: metaEnv['NG_APP_API_BASE_URL'] || 'https://content-creator-be.onrender.com/api' || 'http://localhost:4000/api',
   illustratorTemplateId: metaEnv['NG_APP_ILLUSTRATOR_TEMPLATE_ID'] ?? 'vt_fact_post_1',
 };
